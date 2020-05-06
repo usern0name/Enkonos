90dd6b13715f45421494578ad47500ae09c4ae88
Nikolay <spay1991@i.ua>
2020-05-06 21:22:08 +0300

    123

---
+++ Managers/VolumeManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Cryptography.X509Certificates;
+using AudioSwitcher.AudioApi.CoreAudio;
+using Enkonos.Model;
+
+namespace Enkonos.Managers
+{
+    public class VolumeManager
+    {
+        public ResponseModel VolumeChange(double value)
+        {
+            try
+            {
+                CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+                Debug.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume);
+                defaultPlaybackDevice.Volume = value;
+                return new ResponseModel
+                {
+                    Ok = true,
+                    Data = new {value = value},
+                    Error = null
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseModel
+                {
+                    Ok = false,
+                    Data = null,
+                    Error = ex.Message
+                };
+            }
+
+    }
+}
